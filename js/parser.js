2b32a69277caf47a19b6c0f3eae57fc41c66d438
Drew Young <drewyoung1@gmail.com>
2012-04-06 08:11:54 -0400

    Initial Commit

---
+++ js/parser.js
@@ -0,0 +1,251 @@
+define(['require', 'exports'], function(require, exports) {
+	var quoteStartRegEx = /^(?:'|")/; // Find the start of a string
+	var strRegEx = /^[\w\d\-_]+$/; // Safe character
+	var $CmdRegExp = /(\\\\|[^\\]|^)\$\(/g; // Find $(
+	var backTickRegEx = /(\\\\|[^\\]|^)`/g; // Find `
+	var quoteRegExp = /(\\\\|[^\\]|^)('|")/g;
+	//(?:[^\\]|^)
+
+	RegExp.escape = function(text) { // Escape characters for a RegExp
+		return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+	};
+
+	var Parser = (function() {
+		function Parser(options) {
+			this.history = [];
+		}
+
+		Parser.prototype.parseEnvCMD = function parseEnvCMD(str, env, execCmd) { // Replace $(<cmd>) `<cmd>`
+			var curMatch, endIndex, cmd, re, cmdOutput; // Variables
+
+			$CmdRegExp.lastIndex = 0; // Make sure we start at the start
+
+			while(curMatch = $CmdRegExp.exec(str)) { // Loop through occurences of $(
+				endIndex = this.claimCMDLine(str, { startIndex: curMatch.index + curMatch[1].length + 2 }); // Get the end of command
+
+				cmd = str.slice(curMatch.index + curMatch[1].length + 2, endIndex + 1); // Get the command
+
+				cmdOutput = execCmd(cmd, env, execCmd) + '';
+
+				$CmdRegExp.lastIndex = curMatch.index + curMatch[1].length + 2 + cmdOutput.length; // Search from here TODO: Make this get the end of the output
+				
+				// Replace
+				re = new RegExp('(' + RegExp.escape(str.substring(0, curMatch.index + curMatch[1].length)) + ')' + RegExp.escape(str.substring(curMatch.index + curMatch[1].length, endIndex + 2)) + '(' + RegExp.escape(str.substring(endIndex + 2)) + ')'); // Create a RegExp for it it finds whats before it (capture), it, and whats after it (capture)
+
+				str = str.replace(re, function($A, $1, $2) { // Use that RegExp to replace the command with the output of the command
+					return $1 + cmdOutput + $2; // Whats before it + the output of the command + whats after it
+				});
+			}
+
+			backTickRegEx.lastIndex = 0; // Make sure we start at the start
+		
+			while(curMatch = backTickRegEx.exec(str)) { // Loop through occurences of `
+				endIndex = this.claimCMDLine(str, { startIndex: curMatch.index + ( curMatch.index == 0 ? 1 : 2 ), startedWith: '`' }); // Get the end of command
+
+				cmd = str.slice(curMatch.index + ( curMatch.index == 0 ? 1 : 2 ), endIndex + 1); // Get the command
+
+				backTickRegEx.lastIndex = endIndex; // Search from here TODO: Make this get the end of the output
+
+				cmdOutput = execCmd(cmd, env, execCmd);
+
+				// Replace
+				re = new RegExp('(' + RegExp.escape(str.substring(0, curMatch.index + ( curMatch.index == 0 ? 0 : 1 ))) + ')' + RegExp.escape(str.substring(curMatch.index + ( curMatch.index == 0 ? 0 : 1 ), endIndex + 2)) + '(' + RegExp.escape(str.substring(endIndex + 2)) + ')'); // Create a RegExp for it it finds whats before it (capture), it, and whats after it (capture)
+
+				str = str.replace(re, function($A, $1, $2) { // Use that RegExp to replace the command with the output of the command
+					return $1 + cmdOutput + $2; // Whats before it + the output of the command + whats after it
+				});
+			}
+		
+			return str; // Return the new string
+		};
+
+		Parser.prototype.claimCMDLine = function claimCMDLine(str, options) { // Get the end of a command
+			var index, curChar, $parens, $found, inStr, backTick, backSlash, bang, startedWith, startIndex; // Variablea
+
+			options = options || {}; // Make sure we have options
+			startIndex = options.startIndex || 0; // Make sure we have an index to start at
+			startedWith = options.startedWith || undefined; // Make sure we know what this command was started with
+			$parens = 0; // Number of parenthesies that this is current nested in
+			backTick = false; // If the last character was a backTick `
+			backSlash = false; // If the last character was a backSlash
+			bang = false; // If the last character was a bang !
+	
+			for(index = startIndex; index < str.length; index++) { // Loop through characters
+				curChar = str[index]; // Get the current character
+
+				if(backSlash) backSlash = false; // If the last character was a backSlash make it not a backslash anymore
+		
+				if(curChar == '$') { // If its a $
+					$found = true; // Say we found a $
+				} else if(curChar == '(') { // If its a (
+					if($found) { // If we just found a $
+						$parens++; // We're nesting
+						$found = false; // We have not found any $
+					}
+				} else if(curChar == ')') { // If its a )
+					if($parens > 0) { // If we are nested
+						$parens--; // Un-Nest one level
+					} else if(!inStr) { // We're not nested nor are we inside a string
+						return index - 1; // End of command
+					}
+				} else if(curChar == '"' || curChar == "'") { // If we're starting a string
+					inStr = curChar; // Save the character that started it
+				} else if(inStr /* We do need be in a string to get out of a string */ && curChar == inStr /* Make sure it both starts and ends with the same character */) { // If we're closeing a string
+					inStr = undefined; // No more saved character
+				} else if(strRegEx.test(curChar)) { // Is just a safe character
+					if($found) { // Have we just found a $
+						$found = false; // Not anymore
+					}
+				} else if(curChar == '`') { // Is this a backtick
+					if(startedWith == '`') { // Did this command start with a backtick
+						return index - 1; // End of command
+					} else {
+						if(backTick) backTick = false; // In a backtick so outof a backtick
+						else backTick = true; // Not in a backtick so in a backtick
+					}
+				} else if(curChar == '\\') { // Is this a backSlash
+					index++; // Skip the next character
+					backSlash = true; // We just found a backslash
+				} else if(curChar == '!') { // Is this a bang !
+					if(bang) {
+						// We alread found a bang
+					} else {
+						bang = true; // We just found a bang
+					} 
+				}
+			}
+	
+			return index - 1; // End of command (definitly)
+		};
+
+		Parser.prototype.toCMDLine = function toCMDLine(arr) { // Create a command from the parsed version AKA Reverse the parse
+			return '"' + arr.map(function(v) {return v.replace(/("|')/g, '\\$1')}).join('" "') + '"';
+		};
+
+		function getMatches(re, str, changeLastIndexBy) {
+			var matches = [], match;
+
+			while(match = re.exec(str)) {
+				matches.push(match);
+				re.lastIndex += changeLastIndexBy;
+				re.lastIndex = re.lastIndex >= 1 ? re.lastIndex : 1;
+			}
+
+			return matches;
+		}
+	
+		Parser.prototype.parse = function parse(str, env, execCmd) { // Parse a command
+			var split, curStart, inQuote, curStr, self, origStr, openQuoteMatch, closeQuoteMatch, tmpRe; // Variable definitions
+		
+			self = this; // Save this
+			origStr = str; // Save the original string
+			env = env || {}; // Make sure we have an enviorment
+			execCmd = execCmd || console.log.bind(console); // Make sure we have a command executer
+			str = this.parseEnvCMD(str, env, execCmd).replace(/(\\\\|[^\\]|^)\$([\w\d_\-]+)/g, function($A, $1, $2) { // Replace enviorment variables and embedded commands
+				return $1 + env[$2]; // Return the character before it and the enviorment variables value
+			}).replace(/(\\\\|[^\\]|^)!(\d+|[\$!])/g, function($a, $1, $2) { // Replace history such as !! !$ and !<num>
+				var tmpHistory; // Temporary variable
+			
+				if($2 == '$') { // If this is !$
+					tmpHistory = self.history[0].split; // Get the last command that was parsed
+				
+					return $1 + tmpHistory[tmpHistory.length - 1]; // Return the last argument of it
+				}
+
+				return $1 + self.history[parseInt($2.replace('!', 0))].text; // Get an integer from $1 replacing ! with 0 and get that whole commands text
+			});
+			split = str.split(' '); // Split it up by spaces
+
+			/*str = ;
+
+			str = str.;*/
+
+			for(var i = 0; i < split.length; i++) { // Join them back together as needed
+				curStr = split[i]; // Get the current String
+				
+				if(curStr[curStr.length - 1] == '\\' && curStr[curStr.length - 2] != '\\') { // Join up with the next arg if this one ends in a backslash and that backslash is not escaped
+					var tmpArr = split.splice(i, 2); // Take the two to be joined out
+					var tmpArr2 = tmpArr[0].split(''); // Split this string up into its characters
+					tmpArr2.splice(curStr.length - 1); // Get rid of the last character
+					tmpArr[0] = tmpArr2.join(''); // Put that back into tmpArr
+					split.splice(i, 0, tmpArr.join(' ')); // Add them into the args array
+				}
+				
+				var matches = getMatches(quoteRegExp, curStr, -1), curMatch; // Get all the quotes and maybe also define some variables
+				// (?:[^\\\\]|^)
+				if(!!inQuote && (new RegExp('([^\\\\]|^)(' + RegExp.escape(inQuote) + ')')).test(curStr)) { // Make sure we in a string and that there is a quote of the same type as the on that started the string
+					split.splice(curStart, 0, split.splice(curStart, (i - curStart) + 1).join(' ')); // Join the string together and add it back into the args array
+					inQuote = undefined; // Clear the start character
+					curStart = undefined; // Clear the start index
+					matches.shift(); // Remove the first match (this match here)
+				}
+				
+				if(!inQuote) { // Are we already in a string
+					for(var index = 0; index < matches.length; index++) { // Loop through the quotes
+						curMatch = matches[index]; // Cache the current match
+						
+						if(!inQuote) { // Are we in a string
+							inQuote = curMatch[2]; // Save character that started the string
+							curStart = i; // Save the start index of the string for if the string spans multiple arguments
+						} else if(inQuote == curMatch[2]) { // End of string
+							inQuote = undefined; // Clear the start character
+							curStart = undefined; // Clear the start index
+						}
+					}
+				}
+				
+				/*for(var i = 0; i < matches.length; i++) {
+					quoteRegExp.lastIndex = matches[i].index - 2; // Make sure we start at the start
+					openQuoteMatch = quoteRegExp.exec(curStr); // Find start quote
+					tmpRe = new RegExp('([^\\\\]|^)(' + RegExp.escape(openQuoteMatch[2]) + ')', 'g'); // Create RegExp to match just that on type of quote
+					tmpRe.lastIndex = quoteRegExp.lastIndex + 1; // Make sure we don't get the first match again
+					closeQuoteMatch = tmpRe.exec(curStr); // Find a close quote (maybe?)
+					if(!closeQuoteMatch) { // Did we find a close quote
+						inQuote = openQuoteMatch[2]; // Save the character that started it so then it can only be ended by the quote that started it
+						curStart = i; // Save the start index of it
+					}
+				}*/
+			}
+
+			/* this.parseEnvCMD(str, env, execCmd).replace(/\$([\w\d_\-]+)/g, function($A, $1) {
+				return env[$1];
+			})*/
+
+			if(inQuote) { // Throw an error if its still in a quote
+				throw "Unclosed quotation";
+			}
+
+			/*for(var i = 0; i < split.length; i++) {
+				curStr = split[i]; // Get the currentString
+
+				if(quoteStartRegEx.test(curStr) && curStr[curStr.length - 1] == curStr[0]) { // Check if this string starts and ends with the same quote
+					split[i] = curStr.replace(quoteStartRegEx, ''); // Get rid of the start quote
+					split[i] = split[i].replace(new RegExp(RegExp.escape(curStr[0]) + '$'), ''); // No more end quote, creates a regexp that selects the quote that the string was started with and add on to the end a $
+				}
+			}*/
+
+			for(var i = 0; i < split.length; i++) {
+				curStr = split[i]; // Get the currentString
+
+				split[i] = curStr.replace(/(\\\\|[^\\]|^)(?:\\n|\\r)/g, '\n') // Replace escaped newlines with actual new lines
+					.replace(/(\\\\|[^\\]|^)(?:"|')/g, '$1') // Replace rogue quotes in the middle of strings
+					.replace(/(\\\\|[^\\]|^)(?:"|')/g, '$1') // Replace the ones the above statement missed eg ones right after one another
+					.replace(/(\\\\|[^\\]|^)\\(["'$])/g, '$1$2') // Replace escaped quotes and $ signs with unescaped ones
+					.replace(/\\\\/g, '\\');
+			}
+
+			split.text = origStr; // Return text also
+
+			this.history.unshift({ // Add it into history for use with !$ !! !<num>
+				split: split,
+				text: origStr
+			});
+
+			return split; // Return the arguments such that 0 is usually the command and ones after that are arguments to the command
+		};
+
+		return Parser;
+	})();
+
+	exports.Parser = Parser;
+});
---
+++ js/test.js
@@ -0,0 +1,129 @@
+define(['require', 'exports', 'parser'], function(require, exports, parserJS) {
+	var Parser = parserJS.Parser;
+	var tests = exports.tests = [
+		function basicParse1(parser) { // Test very basic parsing
+			return assert(parser.parse('echo hello'), ['echo', 'hello']);
+		},
+		function basicParse2(parser) { // Some more basic parsing
+			return assert(parser.parse('edit websites/WebCLIOS/index.html'), ['edit', 'websites/WebCLIOS/index.html']);
+		},
+		function basicParse3(parser) { // And even some more almost identical to above
+			return assert(parser.parse('show websites/WebCLIOS/index.html'), ['show', 'websites/WebCLIOS/index.html']);
+		},
+		function history1(parser) { // Test to see if !! to get the whole last parse
+			parser.parse('hello');
+
+			return assert(parser.parse('!!'), ['hello']);
+		},
+		function history2(parser) { // Test to see if !$ correctly gets the last argument of the last command
+			parser.parse('echo hello world');
+
+			return assert(parser.parse('echo !$'), ['echo', 'world']);
+		},
+		function history3(parser) { // Test to see if !<num> works correctly basicly the same as !! but a different index (other than 0)
+			parser.history[33] = { split: ['show', '~/code/cliParser/parser.html'], text: 'show ~/code/cliParser/parser.html' };
+
+			return assert(parser.parse('!33'), ['show', '~/code/cliParser/parser.html']);
+		},
+		function history4(parser) { // Test !$ again this time more realistic
+			parser.parse('ls /media');
+			
+			return assert(parser.parse('cd !$'), ['cd', '/media']);
+		},
+		function history5(parser) { // And again another realistic test
+			parser.parse('ls /media');
+			
+			return assert(parser.parse('ls -lha !$'), ['ls', '-lha', '/media']);
+		},
+		function toCMDLine1(parser) { // Test to see if the toCMDLine will correctly create a command from a parsed array
+			return assert(parser.toCMDLine(parser.parse('echo hello world')), '"echo" "hello" "world"');
+		},
+		function toCMDLine2(parser) { // Test it again now seeing if parsing it after running it through toCMDLine returns the correct result
+			return assert(parser.parse(parser.toCMDLine(['echo', 'hello', 'world'])), ['echo', 'hello', 'world']);
+		},
+		function toCMDLine3(parser) { // Test to see if the reparsed is == to just parsed
+			return assert(parser.parse(parser.toCMDLine(['cat', '/etc/httpd.conf'])), parser.parse('cat /etc/httpd.conf'));
+		},
+		function enviormentVariables1(parser) { // Test to see if variable subsitution works correctly
+			return assert(parser.parse('echo $variable', { variable: "hello world" }), ['echo', 'hello', 'world']);
+		},
+		function enviormentVariables2(parser) { // Test it again
+			return assert(parser.parse('$vim_path', { vim_path: '/usr/bin/vim' }), ['/usr/bin/vim']);
+		},
+		function embeddedCmd1(parser) { // Test embedded commands with the $(<cmd>) syntax
+			return assert(parser.parse('echo $(pwd)', {}, function(cmd, env, execCmd) {
+				return 'cmd';
+			}), ['echo', 'cmd']);
+		},
+		function embeddedCmd2(parser) { // Test it again but with variable subsitution and backtick syntax mixed in
+			return assert(parser.parse('echo $(echo `pwd`/$filename.$filetype)', { filename: 'hello', filetype: 'txt' }, function(cmd, env, execCMD) {
+				return parser.toCMDLine(parser.parse(cmd, env, execCMD));
+			}), ['echo', 'echo', 'pwd/hello.txt']);
+		},
+		function embeddedCmd3(parser) { // Test to see if escaping backslashes works
+			return assert(parser.parse('\\\\$(pwd)', {}, function(cmd, env, execCMD) {
+				return parser.parse(cmd, env, execCMD).join(' ');
+			}), ['\\pwd']);
+		},
+		function string1(parser) { // Test strings to see if they correctly split the command
+			return assert(parser.parse('echo "hello world"'), ['echo', 'hello world']);
+		},
+		function string2(parser) { // Test multiple strings
+			return assert(parser.parse('"hi""hello "'), ['hihello ']);
+		},
+		function string3(parser) { // Test string now with single-quote
+			return assert(parser.parse("echo 'hello world'"), ['echo', 'hello world'])
+		},
+		function string4(parser) {
+			return assert(parser.parse("echo 'hello 'hi''"), ['echo', 'hello hi']);
+		},
+		function escape1(parser) { // Test to see if we can escape quotes
+			return assert(parser.parse('echo "hi \\""'), ['echo', 'hi "']);
+		},
+		function escape2(parser) { // And now testing with $
+			return assert(parser.parse('echo \\$filename'), ['echo', '$filename']);
+		},
+		function escape3(parser) { // Test for a bug i found where escaping backslashes doesn't work (problably because i hadn't implemented it yet)
+			return assert(parser.parse('\\\\"hi "'), ['\\hi ']);
+		}
+	];
+
+	function assert(got, expected) {
+		// console.log(got, expected);
+		
+		if(typeof(got) == 'object' && got.length !== undefined && got.length !== null && typeof(expected) == 'object' && expected.length !== undefined && expected.length !== null) { // Array
+			var good = true;
+
+			for(var i = 0; i < got.length; i++) {
+				if(got[i] !== expected[i]) good = false;
+			}
+
+			return { value: got, expected: expected, good: good };
+		} else if(typeof(got) == 'string' && typeof(expected) == 'string') { // String
+			return { value: got, expected: expected, good: got == expected };
+		}
+	}
+	
+	exports.run = function run() {
+		var good = true, goods = [], tmp;
+
+		for(var i = 0; i < tests.length; i++) {
+			try {
+				if(!(tmp = tests[i](new Parser())).good) {
+					good = false;
+					goods[i] = false;
+					console.log('BAD:', tests[i].name, tmp.value, tmp.expected);
+				} else {
+					goods[i] = true;
+					console.log('GOOD:', tests[i].name);
+				}
+			} catch(ex) {
+				console.log('BAD:', tests[i].name, 'EX:', ex, ex.stack, ex.message);
+			}
+		}
+
+		console.log(good ? 'GOOD' : 'BAD');
+
+		return good;
+	};
+});
